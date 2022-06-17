5d78c60d9d090ac1c04628bfc4ce77726c9ac431
Ihsan06 <ihsan.yilmaz@gmx.de>
2022-06-17 04:40:46 -0700

    BMZ

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+
+
+# from django.db import models
+# from django.db.models import Max
+# from django.utils import timezone
+# from django import forms
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
